7c2dcc52cbf2759ef06cce30fc0c1fa25dc7b8cb
Tomas Doran <tdoran@yelp.com>
2014-02-02 21:59:09 +0000

    More debugging/info

--- JENKINS_BUILD.sh
+++ JENKINS_BUILD.sh
@@ -3,13 +3,22 @@ if [ "$WORKSPACE" == "" ];then
   echo "WORKSPACE ENV variable not set, not in Jenkins - dieing"
   exit 2
 fi
+set -e
+export PATH="/opt/local/bin:/sbin:/usr/sbin:/opt/ruby/bin:$PATH"
+
+echo -n "Cleaning up workspace: "
+rm -rf /opt/puppet /opt/ruby /opt/puppet-omnibus
 rm -rf $WORKSPACE/artifacts
+mkdir $WORKSPACE/artifacts
+echo "Done"
+echo -n "Unpacking bootstrap ruby: "
 pushd /
 tar xzf $WORKSPACE/ruby-*.tgz
 popd
-export PATH="/opt/local/bin:/sbin:/usr/sbin:/opt/ruby/bin:$PATH"
+echo "Done"
+echo "Going for bundle install and build:"
+set -x
 /opt/ruby/bin/bundle install --binstubs
 fakeroot /opt/ruby/bin/bundle exec bin/fpm-cook package recipe-aws.rb
-mkdir $WORKSPACE/artifacts
 mv $WORKSPACE/*.deb $WORKSPACE/*.rpm $WORKSPACE/artifacts/
 
