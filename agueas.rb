4f51a44fa72503f8fb1e9f61e1b9e51670994227
Tomas Doran <tdoran@yelp.com>
2014-02-04 23:01:46 +0000

    Add agueas recipe

---
+++ agueas.rb
@@ -0,0 +1,42 @@
+class Agueas < FPM::Cookery::Recipe
+  description 'Augeas is a configuration editing tool'
+
+  name 'libaugeas'
+  version '1.2'
+  revision 1
+  homepage 'http://augeas.net/'
+  source 'http://download.augeas.net/augeas-1.2.0.tar.gz'
+  sha256 'f4aeb28ebe0b0921920fe1c9b4c016739c25261a15de04cb97db02d669f481e0'
+
+  maintainer '<github@tinycat.co.uk>'
+  vendor     'fpm'
+  license    'The Ruby License'
+
+  section 'interpreters'
+
+  platforms [:ubuntu, :debian] do
+    build_depends 'autoconf',
+                  'bison'
+  end
+
+  platforms [:fedora, :redhat, :centos] do
+    build_depends 'rpmdevtools',
+                  'autoconf',
+                  'bison'
+  end
+
+  def build
+    configure :prefix => destdir,
+              'enable-shared' => true,
+              'disable-install-doc' => true,
+              'with-opt-dir' => destdir
+    make
+  end
+
+  def install
+    make :install
+    # Shrink package.
+    safesystem "find #{destdir} -name '*.so' -or -name '*.so.*' | xargs strip"
+  end
+end
+
