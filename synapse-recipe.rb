775b6069c4c0da12c4e67b015589dbf2c119dbad
Tomas Doran <tdoran@yelp.com>
2014-03-27 04:35:21 +0000

    better version no

--- synapse-recipe.rb
+++ synapse-recipe.rb
@@ -2,7 +2,7 @@ class PuppetOmnibus < FPM::Cookery::Recipe
   homepage 'https://github.com/bobtfish/puppet-omnibus'
   section 'Utilities'
   name 'synapse-omnibus'
-  version '3.0.2'
+  version '0.8.0'
   description 'Synapse Omnibus package'
   revision ENV['BUILD_NUMBER']
 
