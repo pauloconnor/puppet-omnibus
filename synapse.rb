7a8b45e31ee6289c01c0b1237016712d42ea7ed1
Tomas Doran <tdoran@yelp.com>
2014-03-27 05:06:32 +0000

    Try to make the post install hook thing happen, as I think that's what's failign the build

--- synapse.rb
+++ synapse.rb
@@ -11,6 +11,56 @@ class SynapseRecipe < FPM::Cookery::Recipe
   end
 
   def install
+    # Provide 'safe' binaries in /opt/<package>/bin like Vagrant does
+    rm_rf "#{destdir}/../bin"
+    destdir('../bin').mkdir
+    destdir('../bin').install workdir('omnibus.bin'), 'synapse'
+
+    with_trueprefix do
+      create_post_install_hook
+      create_pre_uninstall_hook
+    end
   end
+
+    def create_post_install_hook
+    File.open(builddir('post-install'), 'w', 0755) do |f|
+      f.write <<-__POSTINST
+#!/bin/sh
+set -e
+
+echo "Not setting up binstubs to /usr/bin"
+exit 0
+
+BIN_PATH="#{destdir}/bin"
+BINS="synapse"
+
+for BIN in $BINS; do
+  update-alternatives --install /usr/bin/$BIN $BIN $BIN_PATH/$BIN 100
+done
+
+exit 0
+      __POSTINST
+    end
+  end
+
+  def create_pre_uninstall_hook
+    File.open(builddir('pre-uninstall'), 'w', 0755) do |f|
+      f.write <<-__PRERM
+#!/bin/sh
+set -e
+
+BIN_PATH="#{destdir}/bin"
+BINS="synapse"
+
+if [ "$1" != "upgrade" ]; then
+  for BIN in $BINS; do
+    update-alternatives --remove $BIN $BIN_PATH/$BIN
+  done
+fi
+
+exit 0
+      __PRERM
+    end
+  end 
 
 end
