02af266290c6ccdaf5d7722a67c217216a6c460f
Tomas Doran <tdoran@yelp.com>
2014-04-30 03:24:06 +0100

    Add libvirt to the omnibus

---
+++ libvirt.rb
@@ -0,0 +1,32 @@
+class Ruby193 < FPM::Cookery::Recipe
+  description 'Libvirt'
+
+  name 'libvirt'
+  version '0.10.2'
+  revision 1
+  homepage 'http://libvirt.org/'
+  source 'ftp://libvirt.org/libvirt/libvirt-0.10.2.tar.gz'
+  sha256 '1fe69ae1268a097cc0cf83563883b51780d528c6493efe3e7d94c4160cc46977'
+
+  vendor     'libvirt'
+  license    'LGPL license'
+
+  section 'libraries'
+
+  platforms [:ubuntu, :debian] do
+	build_depends 'build-essential'
+  end
+
+  platforms [:fedora, :redhat, :centos] do
+	build_depends 'gcc', 'gcc-c++', 'make'
+  end
+
+  def build
+    configure :prefix => destdir
+    make
+  end
+
+  def install
+    make :install
+  end
+end
--- recipe.rb
+++ recipe.rb
@@ -27,7 +27,8 @@ class PuppetOmnibus < FPM::Cookery::Recipe
 
   omnibus_package true
   omnibus_dir     "/opt/#{name}"
-  omnibus_recipes 'libaugeas',
+  omnibus_recipes 'libvirt',
+                  'libaugeas',
                   'libyaml',
                   'ruby',
                   'puppet',
