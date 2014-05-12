d754ad674684f4c1140cc9eaa41b38685a2e7a5e
Kyle Anderson <kwa@yelp.com>
2014-05-12 12:05:24 -0700

    Made build script slighly less verbose

--- JENKINS_BUILD.sh
+++ JENKINS_BUILD.sh
@@ -13,11 +13,6 @@ cp -r /package_source/* /package/
 /opt/ruby/bin/bundle install --binstubs --local
 fakeroot /opt/ruby/bin/bundle exec bin/fpm-cook package recipe.rb
 echo "Copying package to the dist folder"
-env
-set -vx
-echo About to put files in /package_dest/
-ls -lha /package_dest/
 cp -v pkg/* /package_dest/
 echo "Package copying worked!"
-ps -ef --forest
-#exit 0
+exit 0
