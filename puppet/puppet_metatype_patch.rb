00275346478e7a01feadc9ef4709374ea002b910
Maksym Melnychok <maksym.melnychok@gmail.com>
2014-07-15 15:58:51 +0200

    don't break mcollective

--- puppet/puppet_metatype_patch.rb
+++ puppet/puppet_metatype_patch.rb
@@ -1,28 +1,32 @@
+require 'rubygems'
 require 'puppet'
-require 'puppet/type'
-require 'puppet/metatype/manager'
 
-# this module wraps original Manager#type method and memoizez it
-module Puppet::MetaType
-module Manager
-  def type(name)
-    return nil if name.to_s.include? ':'
+if Puppet::PUPPETVERSION.to_s =~ /3.6/
+  require 'puppet/type'
+  require 'puppet/metatype/manager'
 
-    @types ||= {}
-    return @types[name] if @types.include? name
+  # this module wraps original Manager#type method and memoizez it
+  module Puppet::MetaType
+  module Manager
+    def type(name)
+      return nil if name.to_s.include? ':'
 
-    if name.is_a? String
-      name = name.downcase.intern
+      @types ||= {}
       return @types[name] if @types.include? name
-    end
 
-    if typeloader.load(name, Puppet.lookup(:current_environment))
-      Puppet.warning "Loaded puppet/type/#{name} but no class was created" unless @types.include? name
-    else
-      @types[name] = nil
-    end
+      if name.is_a? String
+        name = name.downcase.intern
+        return @types[name] if @types.include? name
+      end
 
-    @types[name]
+      if typeloader.load(name, Puppet.lookup(:current_environment))
+        Puppet.warning "Loaded puppet/type/#{name} but no class was created" unless @types.include? name
+      else
+        @types[name] = nil
+      end
+
+      @types[name]
+    end
   end
-end
+  end
 end
