fb759e63f360193e1577b2bf0d1a9846d6c608f5
Maksym Melnychok <maksym@yelp.com>
2014-07-16 07:31:44 -0700

    bump puppet version

--- JENKINS_BUILD.sh
+++ JENKINS_BUILD.sh
@@ -9,7 +9,7 @@ if [ "$BUILD_NUMBER" == "" ];then
 fi
 echo "Going for bundle install and build:"
 
-export PUPPET_VERSION=3.6.2.y3
+export PUPPET_VERSION=3.6.2.y4
 export PUPPET_DASHVER=${PUPPET_VERSION//./-}
 export PUPPET_BUILDPATH=/tmp/puppet.$PUPPET_DASHVER
 
